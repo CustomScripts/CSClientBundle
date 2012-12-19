716b5a84b08ec1a6a1319bafb94a05de90400334
Pierre du Plessis <pierre@pcservice.co.za>
2012-12-19 12:44:30 +0200

    Removed CSMenuBundle dependency

--- src/CS/ClientBundle/Controller/DefaultController.php
+++ src/CS/ClientBundle/Controller/DefaultController.php
@@ -18,11 +18,7 @@ use CS\CoreBundle\Controller\Controller;
 use CS\ClientBundle\DataGrid\Grid;
 use CS\ClientBundle\Entity\Client;
 use CS\ClientBundle\Form\Type\ClientType;
-use CS\MenuBundle\Annotation\Menu;
 
-/**
- * @Menu("CSClientBundle:Main:Sidebar", block="sidebar")
- */
 class DefaultController extends Controller
 {
     /**
