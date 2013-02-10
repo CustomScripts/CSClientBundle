8ca0f462de0878292257d2914e6ffa391095614f
Pierre du Plessis <pierre@pcservice.co.za>
2013-02-10 18:09:20 +0200

    Added status to clients

---
+++ src/CS/ClientBundle/Model/Status.php
@@ -0,0 +1,48 @@
+<?php
+
+/*
+ * This file is part of the CSClientBundle package.
+ *
+ * (c) Pierre du Plessis <info@customscripts.co.za>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace CS\ClientBundle\Model;
+
+/**
+ * This class converts a client status into a label for use with twitter bootstrap
+ *
+ * E.G it will convert 'active' into 'success', so it can be used with then class 'label-success'
+ *
+ * @author Pierre du Plessis
+ */
+class Status {
+
+	/**
+	 * Contains a list of all the statuses and their corresponding labels
+	 *
+	 * @var array
+	 */
+	protected $statusLabels = array(
+									'active' 	=> 'success',
+									'inactive' 	=> 'warning'
+			);
+
+	/**
+	 * Converts a status into a label
+	 *
+	 * @param string $status
+	 * @return string
+	 */
+	public function getStatusLabel($status)
+	{
+		if(isset($this->statusLabels[$status]))
+		{
+			return $this->statusLabels[$status];
+		}
+
+		return 'inverse';
+	}
+}
